cb65259dbb45b949f79e540ea50ab5dc1697c116
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-04 10:34:42 +0700

    update config

--- home.nix
+++ home.nix
@@ -209,8 +209,9 @@
 			];
 
 			general = {
-				gaps_in = 0;
-				gaps_out = 0;
+				gaps_in = 5;
+				gaps_out = 20;
+				no_gaps_when_only = 1;
 				border_size = 2;
 				"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
 				"col.inactive_border" = "rgba(595959aa)";
