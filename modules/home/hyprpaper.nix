098d6fbe4f43843c0b4172c47f1deba91c9c72e7
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-06 19:28:33 +0700

    update config

--- modules/home/hyprpaper.nix
+++ modules/home/hyprpaper.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
 	home.file.".config/wallpapers/calvin-and-hobbes.jpg".source =
 		../../wallpapers/calvin-and-hobbes.jpg;
@@ -6,8 +6,8 @@
 	services.hyprpaper = {
 		enable = true;
 		settings = {
-			preload = [ "~/.config/wallpapers/calvin-and-hobbes.jpg" ];
-			wallpaper = [ ",~/.config/wallpapers/calvin-and-hobbes.jpg" ];
+			preload = [ "${config.home.homeDirectory}/.config/wallpapers/calvin-and-hobbes.jpg" ];
+			wallpaper = [ ",${config.home.homeDirectory}/.config/wallpapers/calvin-and-hobbes.jpg" ];
 			splash = false;
 		};
 	};
