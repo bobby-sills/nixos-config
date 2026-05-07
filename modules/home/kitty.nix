25eeece2e850ff9d48c0ed441d94c5a2776024e1
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-07 10:09:59 +0700

    update config

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -2,8 +2,6 @@
 {
 	programs.kitty = {
 		enable = true;
-		shellIntegration.enableBashIntegration = true;
-		shellIntegration.mode = "no-prompt-mark";
 		font = {
 			name = "Iosevka Nerd Font";
 			size = 12;
