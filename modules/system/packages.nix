8bd66fb107dd723e0c7c4bccfcdc3d730caafb6b
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-08 09:51:12 +0700

    added hledger

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,52 +1,53 @@
 { pkgs, inputs, ... }:
 {
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	users.users.bobby = {
-		isNormalUser = true;
-		extraGroups = [ "wheel" "video" "networkmanager" ];
-		packages = with pkgs; [
-			tree
-		];
-	};
+  users.users.bobby = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "networkmanager" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
 
-	programs.firefox.enable = true;
+  programs.firefox.enable = true;
 
-	programs.obs-studio.enable = true;
+  programs.obs-studio.enable = true;
 
-	environment.systemPackages = with pkgs; [
-		vim
-		wget
-		foot
-		waybar
-		kitty
-		git
-		eza
-		hyprpaper
-		claude-code
-		wofi
-		bemoji
-		wl-clipboard
-		gdu
-		brightnessctl
-		ttyper
-		hyprshot
-		mpv
-		bluetui
-		impala
-		spotify-player
-		ripgrep
-		gh
-		python3
-		inputs.helium-nix.packages.x86_64-linux.default
-		beeper
-		pipes
-		cbonsai
-		unimatrix
-		hyprsunset
-		deno
-		ffmpeg
-		fastfetch
-		google-chrome
-	];
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    foot
+    waybar
+    kitty
+    git
+    eza
+    hyprpaper
+    claude-code
+    wofi
+    bemoji
+    wl-clipboard
+    gdu
+    brightnessctl
+    ttyper
+    hyprshot
+    mpv
+    bluetui
+    impala
+    spotify-player
+    ripgrep
+    gh
+    python3
+    inputs.helium-nix.packages.x86_64-linux.default
+    beeper
+    pipes
+    cbonsai
+    unimatrix
+    hyprsunset
+    deno
+    ffmpeg
+    fastfetch
+    google-chrome
+    hledger
+  ];
 }
