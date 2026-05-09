178757e7d30cd6fef54ae340340ca09cc9335f0c
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-09 12:23:49 +0700

    update config

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -28,4 +28,15 @@
 		enable = true;
 		platformTheme.name = "gtk";
 	};
+
+	xdg.mimeApps = {
+		enable = true;
+		defaultApplications = {
+			"text/html" = "helium.desktop";
+			"x-scheme-handler/http" = "helium.desktop";
+			"x-scheme-handler/https" = "helium.desktop";
+			"x-scheme-handler/about" = "helium.desktop";
+			"x-scheme-handler/unknown" = "helium.desktop";
+		};
+	};
 }
