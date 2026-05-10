2ea2dfebccc96d8334df7e0b4d70356af0facf09
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-10 15:01:19 +0700

    update config

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -58,20 +58,18 @@ in
         scroll-step = 5;
       };
 
-	"battery": {
-		"states": {
+	battery: {
+		states: {
 			// "good": 95,
-			"warning": 30,
-			"critical": 15
+			warning: 30,
+		critical: 15
 		},
-		"format": "{capacity}% {icon}",
-		"format-full": "{capacity}% {icon}",
-		"format-charging": "{capacity}% ",
-		"format-plugged": "{capacity}% ",
-		"format-alt": "{time} {icon}",
-		// "format-good": "", // An empty format will hide the module
-		// "format-full": "",
-		"format-icons": [
+	format: "{capacity}% {icon}",
+		format-full: "{capacity}% {icon}",
+		format-charging: "{capacity}% ",
+		format-plugged: "{capacity}% ",
+		format-alt: "{time} {icon}",
+		format-icons: [
 			" ",
 			" ",
 			" ",
