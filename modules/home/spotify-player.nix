3aa913f178a9e889b66562729c85871ac4e1412d
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-14 20:06:28 +0700

    update config

--- modules/home/spotify-player.nix
+++ modules/home/spotify-player.nix
@@ -1,13 +1,10 @@
 { ... }:
-let
-	g = import ../gruvbox.nix;
-in
 {
 	programs.spotify-player = {
 		enable = true;
 		settings = {
 			notify_transient = true;
-			theme = "gruvbox_dark";
+			theme = "custom";
 			layout = {
 				playback_window_height = 11;
 			};
@@ -25,26 +22,9 @@ in
 
 		themes = [
 			{
-				name = "gruvbox_dark";
-				palette = {
-					background    = g.dark0;
-					foreground    = g.light1;
-					black         = g.dark0;
-					red           = g.neutral_red;
-					green         = g.neutral_green;
-					yellow        = g.neutral_yellow;
-					blue          = g.neutral_blue;
-					magenta       = g.neutral_purple;
-					cyan          = g.neutral_aqua;
-					white         = g.light4;
-					bright_black  = g.gray_245;
-					bright_red    = g.bright_red;
-					bright_green  = g.bright_green;
-					bright_yellow = g.bright_yellow;
-					bright_blue   = g.bright_blue;
-					bright_magenta = g.bright_purple;
-					bright_cyan   = g.bright_aqua;
-					bright_white  = g.light1;
+				name = "custom";
+				component_style = {
+					playback_progress_bar = { fg = "Green"; };
 				};
 			}
 		];
