6fc3ae545b8a0a0a19a19ffbe5e53fb621fb08c5
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-14 19:53:11 +0700

    update config

--- modules/home/spotify-player.nix
+++ modules/home/spotify-player.nix
@@ -1,28 +1,49 @@
 { ... }:
+let
+	g = import ../gruvbox.nix;
+in
 {
-  programs.spotify-player = {
-    enable = true;
-    settings = {
-      notify_transient = true;
-      theme = "gruvbox-fix";
-    };
+	programs.spotify-player = {
+		enable = true;
+		settings = {
+			notify_transient = true;
+			theme = "gruvbox_dark";
+			layout = {
+				playback_window_height = 12;
+			};
+		};
 
-    actions = [
-      {
-        action = "ToggleLiked";
-        key_sequence = "C-l";
-      }
-    ];
+		actions = [
+			{
+				action = "ToggleLiked";
+				key_sequence = "C-l";
+			}
+		];
 
-    themes = [
-      {
-        name = "gruvbox-fix";
-        component_style = {
-          playback_progress_bar = {
-	    fg = "Cyan"; 
-	  };
-        };
-      }
-    ];
-  };
+		themes = [
+			{
+				name = "gruvbox_dark";
+				palette = {
+					background    = g.dark0;
+					foreground    = g.light1;
+					black         = g.dark0;
+					red           = g.neutral_red;
+					green         = g.neutral_green;
+					yellow        = g.neutral_yellow;
+					blue          = g.neutral_blue;
+					magenta       = g.neutral_purple;
+					cyan          = g.neutral_aqua;
+					white         = g.light4;
+					bright_black  = g.gray_245;
+					bright_red    = g.bright_red;
+					bright_green  = g.bright_green;
+					bright_yellow = g.bright_yellow;
+					bright_blue   = g.bright_blue;
+					bright_magenta = g.bright_purple;
+					bright_cyan   = g.bright_aqua;
+					bright_white  = g.light1;
+				};
+			}
+		];
+	};
 }
