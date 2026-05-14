40b83e2e3977cfbe69ed6bc7d0e994ce3abe44f3
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-14 19:13:24 +0700

    update config

--- modules/home/spotify-player.nix
+++ modules/home/spotify-player.nix
@@ -4,7 +4,23 @@
 		enable = true;
 		settings = {
 			notify_transient = true;
+			theme = "gruvbox-fix";
 		};
 
+		actions = [
+			{
+				action = "ToggleLiked";
+				key_sequence = "C-l";
+			}
+		];
+
+		themes = [
+			{
+				name = "gruvbox-fix";
+				component_style = {
+					playback_progress_bar = { fg = "Yellow"; };
+				};
+			}
+		];
 	};
 }
