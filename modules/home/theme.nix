500ff0ad18093b7a6723a4e52a90387354cd9ecc
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-16 10:15:15 +0700

    update config

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -25,6 +25,10 @@ in
 
   gtk = {
     enable = true;
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
   };
