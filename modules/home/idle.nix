66508f7938d999e183154afa4998b6bdd12ff879
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-16 12:25:08 +0700

    update config

--- modules/home/idle.nix
+++ modules/home/idle.nix
@@ -1,8 +1,45 @@
 { ... }:
+let
+  gb = import ../gruvbox.nix;
+  vars = import ../vars.nix;
+in
 {
   services.batsignal.enable = true;
 
-  programs.hyprlock.enable = true;
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      background = [
+        {
+          monitor = "";
+          path = "";
+          color = "rgb(${builtins.substring 1 6 gb.dark0})";
+          blur_passes = 0;
+        }
+      ];
+
+      input-field = [
+        {
+          monitor = "";
+          size = "250, 50";
+          outline_thickness = 2;
+          outer_color = "rgb(${builtins.substring 1 6 gb.bright_orange})";
+          inner_color = "rgb(${builtins.substring 1 6 gb.dark1})";
+          font_color = "rgb(${builtins.substring 1 6 gb.light1})";
+          check_color = "rgb(${builtins.substring 1 6 gb.bright_orange})";
+          fail_color = "rgb(${builtins.substring 1 6 gb.bright_red})";
+          rounding = 10;
+          font_family = vars.monoFont.name;
+          fade_on_empty = true;
+          placeholder_text = "";
+          fail_text = "<i>$FAIL ($ATTEMPTS)</i>";
+          position = "0, 0";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
+  };
 
   services.hypridle = {
     enable = true;
