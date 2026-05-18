a9ce2494a39f6f276e427c9d9bc429b5e743a9db
Bobby Sills <bobbysills@bobbysills.dev>
2026-05-18 15:05:37 +0700

    update config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -201,7 +201,7 @@ in
         "$mainMod, U, exec, case $(powerprofilesctl get) in power-saver) powerprofilesctl set balanced;; balanced) powerprofilesctl set performance;; performance) powerprofilesctl set power-saver;; esac"
         "$mainMod, F, fullscreenstate, 0, 2"
         "$mainMod SHIFT, F, fullscreen, 0"
-        "$mainMod, I, exec, pkill wshowkeys || wshowkeys -a bottom -F '${vars.monoFont.name} ${toString vars.monoFont.size}' -b '${gb.dark0}dd' -f '${gb.light1}ff' -s '${gb.bright_orange}ff'"
+        "$mainMod, I, exec, pkill wshowkeys || wshowkeys -a bottom -F '${vars.monoFont.name} 20' -b '${gb.dark0}dd' -f '${gb.light1}ff' -s '${gb.bright_orange}ff'"
         "$mainMod, E, exec, bemoji"
         "$mainMod, comma, exec, playerctl previous"
         "$mainMod, period, exec, playerctl next"
